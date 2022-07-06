dbf446acb857832939f6a53e4a04118017c5a268
Juan Desimoni <desijuan89@gmail.com>
2022-07-06 20:28:46 -0300

    better error message

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,10 @@
 const { CustomAPIError } = require('../errors/custon-error')
 
 const errorHandler = (err, req, res, next) => {
-  console.log(err);
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   };
-  return res.status(500).json({ msg: 'Oops!' })
+  return res.status(500).json({ msg: err.message })
 };
 
 module.exports = errorHandler;

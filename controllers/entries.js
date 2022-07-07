34d0667747c5242b719d03524ed73e2235157c4b
Juan Desimoni <desijuan89@gmail.com>
2022-07-07 01:21:53 -0300

    minor fix

--- controllers/entries.js
+++ controllers/entries.js
@@ -14,9 +14,7 @@ const createEntry = async (req, res) => {
 const getEntry = async (req, res, next) => {
   const entryID = req.params.id;
   const entry = await Entry.findById(entryID);
-  if (!entry) {
-    throw new CustomAPIError(`No entry with id ${entryID}`, 404)
-  };
+  if (!entry) throw new CustomAPIError(`No entry with id ${entryID}`, 404);
   res.status(200).json({ entry });
 };
 
@@ -26,18 +24,14 @@ const updateEntry = async (req, res) => {
     new: true,
     runValidators: true
   });
-  if (!entry) {
-    throw new CustomAPIError(`No entry with id ${entryID}`, 404)
-  };
+  if (!entry) throw new CustomAPIError(`No entry with id ${entryID}`, 404);
   res.status(200).json({ entry });
 };
 
 const deleteEntry = async (req, res) => {
   const entryID = req.params.id;
   const entry = await Entry.findByIdAndDelete(entryID);
-  if (!entry) {
-    throw new CustomAPIError(`No entry with id ${entryID}`, 404)
-  };
+  if (!entry) throw new CustomAPIError(`No entry with id ${entryID}`, 404);
   res.status(200).json({ entry });
 };
 
